6fd860709a7e3310d52c18c76f669e39c0e92eb1
Tobias Effner <tobias.effner@valtech-mobility.com>
2020-12-04 09:05:04 +0100

    Added gallery

---
+++ static/gallery.js
@@ -0,0 +1,17 @@
+function toggle(imgs) {
+    // Get the expanded image
+    const expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    const imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    if (imgText.innerHTML === imgs.alt) {
+        imgText.innerHTML = ""
+        expandImg.parentElement.style.display = "none";
+        return
+    }
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
